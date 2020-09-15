cdcb236c1d960793ca998551fd91a0a10ea56174
apgit33 <adrienpaturot@yahoo.fr>
2020-09-15 23:46:52 +0200

    Add getName function
    Ajout d'une fonction pour récupere le nom de la catégorie par l'id

--- application/class/Category.php
+++ application/class/Category.php
@@ -15,8 +15,21 @@ class Category {
      */
     static function getAll() {
         $query = "SELECT c_id, c_name FROM ca_category";
-        $response = \classified_ads\Bdd::executeSql($query,[],[]);
+        $response = \classified_ads\Bdd::executeSql($query);
 
         return $response->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Récupère le nom de la catégorie
+     *
+     * @param [int] $id
+     * @return string
+     */
+    static function getName($id){
+        $query = "SELECT c_name FROM ca_category WHERE c_id = :id";
+        $response = \classified_ads\Bdd::executeSql($query,[':id'=>$id],[':id'=>\PDO::PARAM_INT]);
+        $response->fetch(\PDO::FETCH_ASSOC);
+        return $response['c_name'];
     }
 }
